fb48f6ca552abdb3b116fe42923530cdb9cd43de
moonboy13 <conlonkyle@gmail.com>
2013-10-09 00:50:52 -0400

    Now reads in property file and initializes class

--- initialize.py
+++ initialize.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 # Stuff to run on startup
 
+from mono_module import *
+
 def makeBoard():
-    board=[]
-    
+    # Open file and read in the properties
+    propFile=open("properties.txt",'r')
+    props=[]
+    for line in propFile:
+        line=line.strip()
+        vals=line.split('|')
+        if (vals[0] is '# Name'):
+            next
+        else:
+            props.append(Property(vals[0],vals[1],vals[2],vals[3],vals[4],
+                                  vals[5],vals[6],vals[7],vals[8],vals[9],
+                                  vals[10]))
+
+    return props
+
+makeBoard() 
