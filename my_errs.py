0da9764230eff2123b8afcd9f96aef47c1d06fe2
moonboy13 <conlonkyle@gmail.com>
2013-10-31 23:42:36 -0400

    starting on the gui

--- my_errs.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# module to contain my personal errors
-
-class TooManyPlayers(Exception):
-    def __init__(self,players):
-        self.nplayers=str(players)
-
-class tooPoor(Exception):
-    pass
