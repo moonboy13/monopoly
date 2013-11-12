93e20fe136a8d3d8d53fad72061461ee431323ea
moonboy13 <conlonkyle@gmail.com>
2013-11-12 17:06:04 -0500

    Tkinter not working on ubuntu.. set up a virtualenv, not sure how it will work on mac

--- gui/main.py
+++ gui/main.py
@@ -2,9 +2,18 @@
 
 # This module handles the execution of the monopoly game
 
+# Window handling module
+import Tkinter as Tk
+
 # Importing of my modules
 import start_screen as startScreen
 import player_select as playerSelect
 
-# Begin the gamstartScreen.main()e
-startScreen.main()
+# Begin the game
+def main():
+    start_root=Tk.Tk()
+    startScreen.startMenu(start_root)
+    start_root.mainloop()
+
+if __name__ == '__main__':
+    main()
